cd0ea9ac49e6d218d7e3ba26e2408ea1c2f85472
Erik Nomitch <enomitch@gmail.com>
2019-05-22 14:44:44 -0500

    3.7

--- run.sh
+++ run.sh
@@ -5,18 +5,18 @@ whoami
 pwd
 echo
 
-which python
-python --version
-ls -1 /usr/bin | grep python
-ls -1 /usr/local/bin | grep python
+# which python
+# python --version
+# ls -1 /usr/bin | grep python
+# ls -1 /usr/local/bin | grep python
 
-test -d /notebooks && ls -1 /notebooks
-echo
-echo
-ls -1 /root/.fastai
-echo
-echo
-test -d /root/.fastai && ls -1 /root/.fastai
+# test -d /notebooks && ls -1 /notebooks
+# echo
+# echo
+# ls -1 /root/.fastai
+# echo
+# echo
+# test -d /root/.fastai && ls -1 /root/.fastai
 
 echo "Installing requirements..."
 pip install --requirement requirements.txt > /dev/null
@@ -27,4 +27,4 @@ jupyter nbconvert --to python /storage/match_2p0/main.ipynb
 cp /storage/match_2p0/main.py .
 
 echo "Starting..."
-python3 main.py
+python3.7 main.py
