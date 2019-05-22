087f0204b5b5bd1b27536aa0faed3d920d92fdaf
MLWohls <mlwohls2@gmail.com>
2019-05-22 14:47:40 -0400

    update ipython to jupyter for nbconvert

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ pip install --requirement requirements.txt > /dev/null
 echo
 
 echo "Converting..."
-ipython nbconvert --to python /storage/match_2p0/main.ipynb
+jupyter nbconvert --to python /storage/match_2p0/main.ipynb
 cp /storage/match_2p0/main.py .
 
 echo "Starting..."
