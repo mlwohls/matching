6eeea9b95e56b20fac82006e5aa4aadd16311427
Erik Nomitch <enomitch@gmail.com>
2019-05-22 00:07:20 -0500

    + session

--- run.sh
+++ run.sh
@@ -4,8 +4,15 @@
 # ls -1
 # echo "sample artifact" > /artifacts/test.txt
 
+
+echo "Session:"
+pwd
+whoami
+echo
+
 echo "Installing requirements..."
 pip install --upgrade --requirement requirements.txt > /dev/null
+echo
 
 echo "Starting main.py..."
 python main.py
