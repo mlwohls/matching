a97e6421049fa828a930ab25bfc418a60088169e
Erik Nomitch <enomitch@gmail.com>
2019-05-22 11:13:39 -0500

    Convert and run

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+echo "Sesion:"
 whoami
 pwd
+echo
 
 echo "Installing requirements..."
 pip install --upgrade --requirement requirements.txt > /dev/null
 echo
 
-echo "Starting..."
-
+echo "Converting..."
 ipython nbconvert --to python /storage/match_2p0/main.ipynb
 mv /storage/match_2p0/main.py /paperspace
 
+echo "Starting..."
 python main.py
