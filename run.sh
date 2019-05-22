93ea02cc59afdde1b4ae039c54a386935bd10b18
Erik Nomitch <enomitch@gmail.com>
2019-05-22 14:18:15 -0500

    Inspection output

--- run.sh
+++ run.sh
@@ -5,6 +5,19 @@ whoami
 pwd
 echo
 
+which python
+python --version
+ls -1 /usr/bin | grep python
+ls -1 /usr/local/bin | grep python
+
+test -d /notebooks && ls -1 /notebooks
+echo
+echo
+ls -1 /root/.fastai
+echo
+echo
+test -d /root/.fastai && ls -1 /root/.fastai
+
 echo "Installing requirements..."
 pip install --requirement requirements.txt > /dev/null
 echo
