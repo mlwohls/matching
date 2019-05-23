cc0e535eef64645e8931fc1c62614135b51017f1
Erik Nomitch <enomitch@gmail.com>
2019-05-22 23:02:53 -0500

    Take arg for RUN_NUM

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ whoami
 pwd
 echo
 
+echo "RUN_NUM (run.sh): $RUN_NUM"
+echo
+
 echo "Installing requirements..."
 pip install --requirement requirements.txt > /dev/null
 echo
@@ -14,8 +17,4 @@ jupyter nbconvert --to python /storage/match_2p0/main.ipynb
 cp /storage/match_2p0/main.py .
 
 echo "Starting..."
-source /paperspace/experiment_env
-
-echo "RUN_NUM (run.sh): $RUN_NUM"
-
 python3.7 main.py
