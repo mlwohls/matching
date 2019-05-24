6d159ba97312df3b20b5c86420b4bc4dd58c3543
Erik Nomitch <enomitch@gmail.com>
2019-05-23 20:08:23 -0500

    A hyperparameter.sh

---
+++ hyperparameter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "From hyperparameter.sh"
+env
+
+# echo "Installing requirements..."
+# pip install --requirement requirements.txt > /dev/null
+# echo
+
+# echo "Converting..."
+# jupyter nbconvert --to python /storage/match_2p0/main.ipynb
+# cp /storage/match_2p0/main.py .
+
+# echo "Starting..."
+# python3.7 main.py
