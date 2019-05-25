aea86be1d040fdfb874bd77d7c9910d3ade7c295
Erik Nomitch <enomitch@gmail.com>
2019-05-25 13:42:23 -0500

    Merge to run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,57 @@
 #!/bin/bash
 
-echo "Sesion:"
-whoami
-pwd
-echo
+# ------------------------------------------------
+# UTILITY ----------------------------------------
+# ------------------------------------------------
+function _info() {
+    echo $1
+    echo "--------------------------------------------------"
+    env
+    echo "--------------------------------------------------"
+    echo
+    echo
+}
 
-echo "RUN_NUM (run.sh): $RUN_NUM"
-echo
+function _checkpoint() {
+  echo "----> $1"
+}
 
-echo "Installing requirements..."
-pip install --requirement requirements.txt > /dev/null
-echo
+function _install_requirements() {
+  _checkpoint "Installing Python requirements..."
+  pip install --requirement requirements.txt > /dev/null
+  echo
+}
 
-echo "Converting..."
-jupyter nbconvert --to python /storage/match_2p0/main.ipynb
-cp /storage/match_2p0/main.py .
+function _convert_main() {
+  _checkpoint "Converting main..."
+  jupyter nbconvert --to python /storage/match_2p0/main.ipynb
+  cp /storage/match_2p0/main.py .
+}
 
-echo "Starting..."
-python3.7 main.py
+function _run_main() {
+  _checkpoint "Running main.py..."
+  python3.7 main.py
+}
+
+# ------------------------------------------------
+# MAIN -------------------------------------------
+# ------------------------------------------------
+case $1 in
+  worker)
+    _info "MULTI-NODE: WORKER"
+    ;;
+  parameter-server)
+    _info "MULTI-NODE: PARAMETER SERVER"
+    ;;
+  job|"")
+    _info "SINGLE-NODE: JOB"
+    _install_requirements
+    _convert_main
+    _run_main
+    ;;
+  *)
+    echo "fatal: Invalid run type supplied."
+    exit 1
+    ;;
+esac
+
